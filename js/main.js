4fd076932eed493e90dace2cad5ef89f8aa8de22
Jax Duan <j_duan168326@fanshaweonline.ca>
2023-11-21 02:49:03 -0500

    made materials list and hotspots images
    added materials list and show the images in the hotspots

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
   //variables
   const model = document.querySelector("#model");
   const hotspots = document.querySelectorAll(".Hotspot");
+
+  const marterialsCon = document.querySelector("#marterialsCon");
+
   const spinner = `<svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
   viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
   <path fill="#333" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
@@ -18,8 +21,6 @@
   </svg>`;
 
 
-
-
     //This information needs to be removed then pulled with an AJAX Call using the Fetch API
     //this is the api url https://swiftpixel.com/earbud/api/materials"
 
@@ -31,25 +32,8 @@
   }
 
   function loadInfoBoxes() {
-
-    //make AJAX call here
-
-    // infoBoxes.forEach((infoBox, index) => {
-    //   let selected = document.querySelector(`#hotspot-${index+1}`);
-      
-    //   const titleElement = document.createElement('h2');
-    //   titleElement.textContent = infoBox.title;
-
-    //   const textElement = document.createElement('p');
-    //   textElement.textContent = infoBox.text;
-
-    //   selected.appendChild(titleElement);
-    //   selected.appendChild(textElement);
-    // });
-
-
     function getData() {
-    // model.innerHTML = spinner;
+    //model.innerHTML = spinner;
     fetch("https://swiftpixel.com/earbud/api/infoboxes")
     .then(response => response.json())
     .then(info => {
@@ -64,12 +48,12 @@
       const textElement = document.createElement('p');
       textElement.textContent = infoBox.description;
 
-      // const imgElement = document.createElement('img');
-      // imgElement.src = infoBox.thumbnail;
+       const imgElement = document.createElement('img');
+       imgElement.src = `images/${infoBox.thumbnail}`; //show the local images
 
       info.appendChild(titleElement);
       info.appendChild(textElement);
-      // info.appendChild(imgElement);
+      info.appendChild(imgElement);
     });
 
     // model.appendChild(selected);
@@ -81,6 +65,44 @@
     getData();
   }
   loadInfoBoxes();
+
+
+  function loadMateraialsBox(){
+    function getData() {
+    fetch("https://swiftpixel.com/earbud/api/materials")
+    .then(respone => respone.json())
+    .then(materials => {
+      console.log(materials);
+
+      let ul = document.createElement("ul")
+
+      materials.forEach((materialsBox)=> {
+        const li = document.createElement("li");
+
+        const h3 = document.createElement("h3");
+        h3.textContent = materialsBox.heading;
+        
+        const p = document.createElement("p");
+        p.textContent = materialsBox.description;
+
+
+        li.appendChild(h3);
+        li.appendChild(p);
+        ul.appendChild(li);
+
+        marterialsCon.appendChild(ul);
+      });
+    })
+       .catch(error => console.error(error)); //catch and report any errors
+
+    }
+    getData();
+  }
+  loadMateraialsBox();
+  
+
+
+
 
 
  
